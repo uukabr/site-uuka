d8976723a240eebde32172542008db68655af981
flavia-dantas <flavia.dantas2@hotmail.com>
2025-10-10 21:30:02 -0300

    feat: create leadership section

--- src/sections/Leadership/leadership-section.tsx
+++ src/sections/Leadership/leadership-section.tsx
@@ -1,7 +1,13 @@
 export function LeadershipSection() {
   return (
-    <div>
-      <h2>diretoria</h2>
-    </div>
+    <section id="diretoria" className="pt-[72px] h-screen w-full bg-black text-white snap-start">
+      <div className="h-[calc(100vh-72px)] w-full flex flex-col justify-between p-6 md:p-12 lg:p-16">
+
+
+        <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-semibold text-[#F59F23]">
+          diretoria
+        </h2>
+      </div>
+    </section>
   );
 }
