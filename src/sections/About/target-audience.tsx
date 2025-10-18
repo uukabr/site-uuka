6d453081f3cb6a48c006503ea51da344ebd81d08
flavia-dantas <flavia.dantas2@hotmail.com>
2025-10-17 21:35:52 -0300

    fix: remove unnecessary snap-start class

--- src/sections/About/target-audience.tsx
+++ src/sections/About/target-audience.tsx
@@ -2,7 +2,7 @@
 
 export default function TargetAudience() {
   return (
-    <section id="publico-alvo" className="pt-[72px] h-screen w-full snap-start bg-[#F59F23] text-white">
+    <section id="publico-alvo" className="pt-[72px] h-screen w-full  bg-[#F59F23] text-white snap-start">
       <div className="h-[calc(100vh-72px)] w-full flex flex-col justify-between p-6 md:p-12 lg:p-16">
         <div className="max-w-sm py-6">
           <p className="text-sm md:text-base lg:text-lg mb-4 leading-relaxed">
